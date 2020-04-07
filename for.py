e7ffdfed4e29ab21511dfe7f457524a47ca3152f
akannshu <akanshuraj@gmail.com>
2020-04-07 23:34:40 +0530

    first commit

---
+++ for.py
@@ -0,0 +1,31 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# num = input()
+# div = input()
+#
+# print(num//div)
+# print(num%div)
+# print(f'({num}, {div})')
+
+# var = "hi"
+# """ Hllop
+# dfngdlfg
+# dgjldfjg"""
+#
+# print(var)
+
+
+# def gcd(a,b):
+#     if(a==0):
+#         return b
+#     return gcd(b%a,a)
+#
+#
+# print(gcd(60,15))
+
+# from itertools import repeat
+# var = repeat(4,100)
+# print(var)
+
+k = int(input())
+for i in range(1,11):
+    print(f"{k} x {i} = {i*k}")
